643aaf096f5f39c76adf6e2d2f77b16b6d68de56
Jeseekia <jeseekia@gmail.com>
2021-05-05 19:53:13 -0400

    Render some starting Project fields

--- themes/legendary-poseidon/single-project.php
+++ themes/legendary-poseidon/single-project.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-  
+  <?php
+    if(have_posts()):
+      while(have_posts()): the_post();
+        the_title('<h1>', '</h1>');
+        the_content('<p>', '<p>'); ?>
+        <a href="<?php the_field('url') ?>">View live</a>
+      <?php endwhile;
+    else:
+      _e("No matching pages found", "textdomain");
+    endif;
+   ?>
 <?php get_footer(); ?>
