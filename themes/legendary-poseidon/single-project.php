57ee8299f27ac6b1d4c33f592c990c2234f1cabb
Jeseekia <jeseekia@gmail.com>
2021-05-28 00:25:12 -0400

    Add preview image to project post type

--- themes/legendary-poseidon/single-project.php
+++ themes/legendary-poseidon/single-project.php
@@ -3,10 +3,13 @@
     if(have_posts()):
       while(have_posts()): the_post();
         the_title('<h1>', '</h1>');
-        the_content('<p>', '<p>'); ?>
-        <a href="<?php the_field('live-url') ?>">View live</a>
+        the_content('<p>', '</p>'); ?>
+        <?php if(get_field('preview')) {?>
+          <img class="project-preview" src="<?php the_field('preview') ?>" alt="" />
+        <?php } ?>
+        <a href="<?php the_field('live-url') ?>" target="_blank">View live</a>
         <?php if(get_field('code-url')) { ?>
-          <a href="<?php the_field('code-url') ?>">View code</a>
+          <a href="<?php the_field('code-url') ?>" target="_blank">View code</a>
         <?php  }?>
       <?php endwhile;
     else:
